0bdf256fc6f0922e117f567a34c74b7330539a43
Frank Kloeker <f.kloeker@telekom.de>
2016-01-30 12:13:00 +0100

    change project-type to sync translation files into the right directory structure

--- cron/zanata-sync.sh
+++ cron/zanata-sync.sh
@@ -1,21 +1,34 @@
 #!/bin/sh
 
 # fetch new po files for spezific lang
-/opt/zanata/zanata-cli-3.8.1/bin/zanata-cli -B -q pull  -l de
+/opt/zanata/zanata-cli-3.8.1/bin/zanata-cli -B -q pull
+
+# working dir (configured in zanata.xml
+cd trans
 
 # compile mo files
-for file in `find trans -name "*.po"` ; do msgfmt -o `echo $file | sed 's/\.po$/.mo/'` $file ; done
+for file in `find . -name "*.po"` ; do msgfmt -o `echo $file | sed 's/\.po$/.mo/'` $file ; done
 
 # backup curret data
-tar cvfz backup_`date +%s`.tgz /opt/stack/horizon/horizon/locale/de/LC_MESSAGES /opt/stack/horizon/openstack_dashboard/locale/de/LC_MESSAGES
+# tar cfz backup_`date +%s`.tgz /opt/stack/horizon/horizon/locale/ /opt/stack/horizon/openstack_dashboard/locale/
 
-# delete staff
-rm -f /opt/stack/horizon/horizon/locale/de/LC_MESSAGES/* 
-rm -f /opt/stack/horizon/openstack_dashboard/locale/de/LC_MESSAGES/*
+# read trans
+ls | while read i 
 
-# copy new content
-cp trans/horizon/locale/* /opt/stack/horizon/horizon/locale/de/LC_MESSAGES/
-cp trans/openstack_dashboard/locale/* /opt/stack/horizon/openstack_dashboard/locale/de/LC_MESSAGES/
+do 
+
+# go to the language directory and copy only exists files into devstack installation
+cd $i
+for file in $(find . -type f -prune | cut -b3-)
+do 
+  dfile=`echo "/opt/stack/horizon/$file" | sed "s/\(.*\)\//\1\/$i\/LC_MESSAGES\//"`
+  if [ -f $dfile ]; then
+    cp $file ${dfile}
+  fi
+done
+cd ..
+
+done
 
 # reload apache
 sudo apache2ctl graceful
